999f5a84949000300eda73fa62fbdf32c0b711dd
耿小泰 <clawpo@foxmail.com>
2017-06-13 00:30:14 +0800

    add res

--- app/src/main/java/cn/ucai/mifareultralightc/MainActivity.java
+++ app/src/main/java/cn/ucai/mifareultralightc/MainActivity.java
@@ -1,13 +1,13 @@
 package cn.ucai.mifareultralightc;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_menu);
     }
 }
