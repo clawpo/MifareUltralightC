f4defa1b2ee915a4cc7d467d3a618eac53d341c7
耿小泰 <clawpo@foxmail.com>
2017-06-13 17:25:07 +0800

    添加ButterKnife的依赖

--- app/src/main/java/cn/ucai/mifareultralightc/MainActivity.java
+++ app/src/main/java/cn/ucai/mifareultralightc/MainActivity.java
@@ -3,11 +3,33 @@ package cn.ucai.mifareultralightc;
 import android.app.Activity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
 public class MainActivity extends Activity {
+    Unbinder bind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        bind = ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.buttonMainReadTag) void onShowReadTag(){
+
+    }
+
+    @OnClick(R.id.buttonMainWriteTag) void onShowWriteTag(){
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (bind!=null){
+            bind.unbind();
+        }
     }
 }
