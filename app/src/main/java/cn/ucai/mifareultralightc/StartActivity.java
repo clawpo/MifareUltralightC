71f2ace89b712278de2b9d1f7c26224ffb574e43
耿小泰 <clawpo@foxmail.com>
2017-06-20 14:20:36 +0800

    添加定制的主页面

---
+++ app/src/main/java/cn/ucai/mifareultralightc/StartActivity.java
@@ -0,0 +1,19 @@
+package cn.ucai.mifareultralightc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by clawpo on 2017/6/20.
+ */
+
+public class StartActivity extends Activity {
+    private static final String TAG = "StartActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+}
